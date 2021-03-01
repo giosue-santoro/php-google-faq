e8782835ddd85cf5631aa39972bf4a323f5ebefd
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-03-01 16:35:57 +0100

    nav

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="dist/app.css">
+    <title>php-google-faq</title>
+</head>
+<body>
+    
+    <header>
+        <nav class="nav-top">
+            <div class="left">
+                <ul>
+                    <li><img src="img/logo-google.jpg" alt="logo"></li>
+                    <li>Privacy e Termini</li>
+                </ul>
+            </div>
+            <div class="right">
+                <ul>
+                    <li><img src="img/hamburger.svg" alt=""></li>
+                </ul>
+            </div>
+        </nav>
+
+        <nav class="bottom">
+            <ul>
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li>Domande frequenti</li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
